adee902125ced646f2beed61e297d6b5602ea48b
EvertonHimself <evertondeoliveira@gmail.com>
2023-05-30 17:21:16 -0300

    Criada a conexão com o banco de dados.
    Criei a conexão com o banco de dados e configurei o banco de dados.

---
+++ sistema-gestao-estudantes/MEU_BD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace sistema_gestao_estudantes
+{
+    // A classe do nosso banco de dados.
+    internal class MEU_BD
+    {
+        // O "objeto" que representa a nossa conexão com o banco de dados.
+        private MySqlConnection conexao = new MySqlConnection("datasource=localhost;username=root;password=;database=t4_sga_bd");
+        
+    }
+}
