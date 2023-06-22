a4fc73912e7cb450101ace2aae6b6a2fe55e98a8
mucamarcondes <murilomarcondes2014@gmail.com>
2023-06-22 13:28:52 -0300

    .

---
+++ student-manager/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace student_manager
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Login_Form());
+            Login_Form telaDeLogin = new Login_Form();
+
+            if (telaDeLogin.ShowDialog() == DialogResult.OK)
+            {
+                Application.Run(new FormPrincipal());
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
--- student-manager/Properties/AssemblyInfo.cs
+++ student-manager/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associadas a um assembly.
-[assembly: AssemblyTitle("sistema-gestao-estudantes")]
+[assembly: AssemblyTitle("student-manager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("sistema-gestao-estudantes")]
+[assembly: AssemblyProduct("student-manager")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
-[assembly: Guid("8302314c-c919-468e-9a47-e2e82f572997")]
+[assembly: Guid("5c009366-1de0-4398-a396-47cca162d179")]
 
 // As informações da versão de um assembly consistem nos quatro valores a seguir:
 //
