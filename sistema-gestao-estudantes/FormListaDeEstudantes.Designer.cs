3c31d9b0f04fa91dfc582944cafacced2e599439
EvertonHimself <evertondeoliveira@gmail.com>
2023-08-17 15:19:51 -0300

    Criado o método para atualizar a lista
    Criado o método para atualizar a lista de estudantes.

--- sistema-gestao-estudantes/FormListaDeEstudantes.Designer.cs
+++ sistema-gestao-estudantes/FormListaDeEstudantes.Designer.cs
@@ -28,29 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dataGridViewLista = new System.Windows.Forms.DataGridView();
+            this.buttonAtualizar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLista)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dataGridViewLista
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(775, 350);
-            this.dataGridView1.TabIndex = 0;
+            this.dataGridViewLista.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewLista.Location = new System.Drawing.Point(13, 13);
+            this.dataGridViewLista.Name = "dataGridViewLista";
+            this.dataGridViewLista.Size = new System.Drawing.Size(775, 350);
+            this.dataGridViewLista.TabIndex = 0;
+            this.dataGridViewLista.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewLista_CellContentClick);
+            this.dataGridViewLista.DoubleClick += new System.EventHandler(this.dataGridViewLista_DoubleClick);
             // 
-            // button1
+            // buttonAtualizar
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(13, 370);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(775, 68);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Atualizar";
-            this.button1.UseVisualStyleBackColor = false;
+            this.buttonAtualizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.buttonAtualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAtualizar.Location = new System.Drawing.Point(13, 370);
+            this.buttonAtualizar.Name = "buttonAtualizar";
+            this.buttonAtualizar.Size = new System.Drawing.Size(775, 68);
+            this.buttonAtualizar.TabIndex = 1;
+            this.buttonAtualizar.Text = "Atualizar";
+            this.buttonAtualizar.UseVisualStyleBackColor = false;
+            this.buttonAtualizar.Click += new System.EventHandler(this.buttonAtualizar_Click);
             // 
             // FormListaDeEstudantes
             // 
@@ -58,19 +62,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Lime;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonAtualizar);
+            this.Controls.Add(this.dataGridViewLista);
             this.Name = "FormListaDeEstudantes";
             this.Text = "FormListaDeEstudantes";
             this.Load += new System.EventHandler(this.FormListaDeEstudantes_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLista)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridViewLista;
+        private System.Windows.Forms.Button buttonAtualizar;
     }
 }
--- sistema-gestao-estudantes/FormListaDeEstudantes.cs
+++ sistema-gestao-estudantes/FormListaDeEstudantes.cs
@@ -18,13 +18,34 @@ namespace sistema_gestao_estudantes
             InitializeComponent();
         }
 
+        Estudante estudante = new Estudante();
+
         private void FormListaDeEstudantes_Load(object sender, EventArgs e)
         {
             // Preenche o "DataGridView" com os dados dos estudantes.
             MySqlCommand comando = new MySqlCommand("SELECT * FROM `estudantes`");
-            dataGridView1.ReadOnly = true;
+            dataGridViewLista.ReadOnly = true;
             DataGridViewImageColumn colunaDeFotos = new DataGridViewImageColumn();
+            dataGridViewLista.RowTemplate.Height = 80;
+            dataGridViewLista.DataSource = estudante.getEstudantes(comando);
+            colunaDeFotos = (DataGridViewImageColumn)dataGridViewLista.Columns[7];
+            colunaDeFotos.ImageLayout = DataGridViewImageCellLayout.Stretch;
+            dataGridViewLista.AllowUserToAddRows = false;
+        }
 
+        private void dataGridViewLista_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridViewLista_DoubleClick(object sender, EventArgs e)
+        {
+            // Abre o estudante selecionado.
+        }
+
+        private void buttonAtualizar_Click(object sender, EventArgs e)
+        {
+            // Atualizar a lista de estudantes.
         }
     }
 }
